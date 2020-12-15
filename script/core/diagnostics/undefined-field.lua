df924b791568c431a9dc30ffa40fb4002cecad41
uhziel <uhziel@gmail.com>
2020-12-15 16:22:50 +0800

    git merge diagnostic-undefined-field

--- script/core/diagnostics/undefined-field.lua
+++ script/core/diagnostics/undefined-field.lua
@@ -53,12 +53,9 @@ return function (uri, callback)
         local fields = {}
         local empty = true
         for _, docClass in ipairs(allDocClass) do
-            local refs = vm.getFields(docClass, 0)
+            local refs = vm.getFieldsOfDocClassAnyNotGet(docClass)
 
             for _, ref in ipairs(refs) do
-                if ref.type == 'getfield' or ref.type == 'getmethod' then
-                    goto CONTINUE
-                end
                 local name = vm.getKeyName(ref)
                 if not name or vm.getKeyType(ref) ~= 'string' then
                     goto CONTINUE
@@ -68,7 +65,7 @@ return function (uri, callback)
                 ::CONTINUE::
             end
         end
-       
+
         if empty then
             return nil
         else
@@ -93,11 +90,19 @@ return function (uri, callback)
 
         if not fields[fieldName] then
             local message = lang.script('DIAG_UNDEF_FIELD', fieldName)
-            callback {
-                start   = src.start,
-                finish  = src.finish,
-                message = message,
-            }
+            if src.type == 'getfield' then
+                callback {
+                    start   = src.field.start,
+                    finish  = src.field.finish,
+                    message = message,
+                }
+            elseif src.type == 'getmethod' then
+                callback {
+                    start   = src.method.start,
+                    finish  = src.method.finish,
+                    message = message,
+                }
+            end
         end
     end
     guide.eachSourceType(ast.ast, 'getfield', checkUndefinedField);
